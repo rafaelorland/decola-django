775f2ee67ea55dc76a60b0cc21c28a70d2608552
Rafael Orlando <rafael.orlando2023@gmail.com>
2024-04-22 00:35:57 -0300

    decola-site

---
+++ decola/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def helloworld(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def home(request):
+    return render(request, 'home/pages/home.html')
