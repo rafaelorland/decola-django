43db1beabc0982fa3905522403307bc9f2eab626
Rafael Orlando <rafael.orlando2023@gmail.com>
2024-04-27 04:10:52 -0300

    update 1.0

--- decola/settings.py
+++ decola/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-woaut0ojb9rt$khe+ok)uz+*@f4(ee0^lt#g1zge)%ryutlzt@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["decola-web.com"]
+
+CSRF_TRUSTED_ORIGINS = ['https://decola-web.com']
 
 
 # Application definition
