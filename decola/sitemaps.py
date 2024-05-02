4bf000c389d5d89564b793788356f06725d8c023
Rafael Orlando <rafael.orlando2023@gmail.com>
2024-05-02 17:50:20 -0300

    data fix bug sitemap

--- decola/sitemaps.py
+++ decola/sitemaps.py
@@ -1,7 +1,28 @@
 from django.contrib import sitemaps
-from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from django.contrib.sitemaps import Sitemap
 from blog.models import Post
+from django.urls import reverse
+from datetime import datetime
+
+class CustomSitemap(Sitemap):
+    changefreq = 'daily'
+    priority = 1.0
+
+    def items(self):
+        # Retorna uma lista de URLs que você deseja incluir no sitemap
+        return ['home', 'helloworld', 'blog_post']
+
+    def lastmod(self, obj):
+        # Define manualmente a data de modificação para cada URL
+        if obj == 'home':
+            return datetime(2024, 5, 1)
+        elif obj == 'helloworld':
+            return datetime(2024, 5, 1)
+
+    def location(self, obj):
+        # Gera automaticamente a URL para cada item usando a função reverse()
+        return reverse(obj)
 
 class PostSitemap(sitemaps.Sitemap):
     changefreq = "weekly"
@@ -15,14 +36,3 @@ class PostSitemap(sitemaps.Sitemap):
 
     def location(self, obj):
         return reverse('blog_detail', args=[obj.slug])
-
-class PageSitemap(Sitemap):
-    priority = 1.0
-    changefreq = 'daily'
-
-    def items(self):
-        return ['home', 'helloworld']
-
-    def location(self, item):
-        # Retorna a URL da página inicial usando o nome da URL
-        return reverse(item)
--- decola/urls.py
+++ decola/urls.py
@@ -21,13 +21,13 @@ from django.conf.urls.static import static
 from decola.views import helloworld, home
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
-from .sitemaps import PostSitemap, PageSitemap
+from .sitemaps import PostSitemap, CustomSitemap
 
 handler404 = 'blog.views.not_found_404'
 
 
 sitemaps = {
-    'pages': PageSitemap,
+    'custom': CustomSitemap,
     'posts': PostSitemap,
 }
 
